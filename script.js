c5df32b46f8786cfc6c21761589a63c22e17e5ba
Eli <109231330+theLadEli@users.noreply.github.com>
2023-07-24 17:04:14 +0100

    Finished Dashboard and Started Add Rooms page

---
+++ script.js
@@ -0,0 +1,29 @@
+var selectedSpaceList = [
+    {
+        icon: "Assets/Space Icons/Kitchen.svg",
+        name: "Kitchen"
+    },
+    {
+        icon: "Assets/Space Icons/Living Room.svg",
+        name: "Living Room"
+    },
+    {
+        icon: "Assets/Space Icons/Bedroom.svg",
+        name: "Bedroom"
+    }
+];
+
+
+function generateCard(item) {
+    $(".selected-sc").append(`
+        <div class="space-card">
+            <img src="${item.icon}">
+            <div class="sc-text">
+                <h3>${item.name}</h3>
+                <p class="sc-action">Select Options</p>
+            </div>
+        </div>
+    `)
+}
+
+selectedSpaceList.forEach(generateCard);
